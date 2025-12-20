e00db66a33cd55a6f927e78bed2efc4871e4a654
Reza Hashemi <rezahashemipersonalemail@gmail.com>
2025-12-20 15:49:50 +0330

    fix: replace joson_logger with jsonlogger

--- app/logging/logger.py
+++ app/logging/logger.py
@@ -1,11 +1,11 @@
 import logging
-from python_json_logger import json_logger
+from python_json_logger import jsonlogger
 
 def setup_logging():
     logger = logging.getLogger("movie_rating")
     logger.setLevel(logging.INFO)
 
-    formatter = json_logger.JsonFormatter(
+    formatter = jsonlogger.JsonFormatter(
         fmt='%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(funcName)s)'
     )
 
