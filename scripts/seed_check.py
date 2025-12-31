f96d859fb3b587439beb1bdb7e6da4c2a4b12dfd
Reza Hashemi <rezahashemipersonalemail@gmail.com>
2026-01-01 02:25:32 +0330

    fix: debuging seed_check file

--- scripts/seed_check.py
+++ scripts/seed_check.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from sqlalchemy.orm import Session
 from app.db.database import SessionLocal
 from app.models.movies import Movie
